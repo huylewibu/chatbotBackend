89dac6de67b871fcb6d0bc4bdd076046c2e46125
ADMIN-PC\ad <abc>
2025-01-17 17:50:02 +0700

    add

--- chatbotLogic/logic/gemini.py
+++ chatbotLogic/logic/gemini.py
@@ -1,13 +1,15 @@
 import google.generativeai as genai
 import os
 from dotenv import load_dotenv
-from openai import OpenAI
-import gradio as gr
+import openai
 
 load_dotenv()
 
 BASE_URL = os.getenv("BASE_URL")
 API_KEY = os.getenv("GOOGLE_API_KEY")
+
+openai.api_key = API_KEY
+openai.api_base = BASE_URL
 
 # Cấu hình generation
 generation_config = {
@@ -34,10 +36,10 @@ safety_settings = [
 #         generation_config=generation_config,
 #         # system_instruction="This is a new chat, and user ask you. No rely and comment just answer for this chat, Max length is 20 characters and Vietnamese name is preferred"
 #     )
-client = OpenAI(
-    base_url=BASE_URL,
-    api_key=API_KEY
-)
+# client = OpenAI(
+#     base_url=BASE_URL,
+#     api_key=API_KEY
+# )
 
 def chat_logic(message, chat_history=[], is_rename_prompt=False):
     try:
@@ -53,7 +55,7 @@ def chat_logic(message, chat_history=[], is_rename_prompt=False):
             prompt = system_instruction.format(content=message)
             messages.append({"role": "system", "content": prompt})
             
-        chat_completion = client.chat.completions.create(
+        chat_completion = openai.ChatCompletion.create(
             model="gemma2-9b-it",
             messages=messages,
             stream=False,
