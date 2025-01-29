03b181835b6b61bf8b59d505f8e857641bd38c9e
ADMIN-PC\ad <abc>
2025-01-30 02:37:03 +0700

    update backend

--- chatbotLogic/logic/gemini.py
+++ chatbotLogic/logic/gemini.py
@@ -1,21 +1,23 @@
 import google.generativeai as genai
 import os
+import absl.logging
 from dotenv import load_dotenv
-import openai
+# import openai
+
+absl.logging.set_verbosity(absl.logging.ERROR)
 
 load_dotenv()
 
 BASE_URL = os.getenv("BASE_URL")
 API_KEY = os.getenv("GOOGLE_API_KEY")
 
-openai.api_key = API_KEY
-openai.api_base = BASE_URL
+genai.configure(api_key=API_KEY)
 
 # Cấu hình generation
 generation_config = {
     "temperature": 1,
     "top_p": 0.95,
-    "top_k": 40,
+    "top_k": 64,
     "max_output_tokens": 8192,
     "response_mime_type": "text/plain"
 }
@@ -32,29 +34,34 @@ safety_settings = [
     },
 ]
 
+model = genai.GenerativeModel(
+    model_name="gemini-exp-1206",
+    generation_config=generation_config,
+    safety_settings=safety_settings,
+)
+
 def chat_logic(message, chat_history=[], is_rename_prompt=False):
     try:
-        messages = [{"role": msg["role"], "content": msg["content"]} for msg in chat_history]
-        messages.append({"role": "user", "content": message})
+        gemini_chat_history = [
+            {"role": "user" if msg["role"] == "user" else "model", "parts": [msg["content"]]}
+            for msg in chat_history
+        ]
+
+        gemini_chat_history.append({"role": "user", "parts": [message]})
         
         if is_rename_prompt:
-            prompt = (
-                f"This is a new chat, and user asked: {message}. "
-                "Provide a suitable name for the chat. No reply or comment, "
-                "just the name in maximum 20 characters, and Vietnamese preferred."
+            system_prompt  = (
+                f"This is a new chat, and user asked: '{message}'. "
+                "Based on this initial message, suggest a concise and relevant title for the chat. "
+                "The title should reflect the likely topic or theme of the conversation, "
+                "be in the same language as the user's message, and be no longer than 20 characters. "
+                "Only provide the title, no other text or explanation."
             )
-            messages.append({"role": "system", "content": prompt})
+            gemini_chat_history.append({"role": "user", "parts": [system_prompt]})
             
-        chat_completion = openai.ChatCompletion.create(
-            model="gemma2-9b-it",
-            messages=messages,
-            stream=False,
-        )
-
-        bot_response = chat_completion.choices[0].message.content.strip()
-        return {"bot_response": bot_response}
+        chat_session = model.start_chat(history=gemini_chat_history)
+        response = chat_session.send_message(message)
+        return {"bot_response": response.text.strip()}
     except Exception as e:
         print("Error:", str(e))
         return {"bot_response": "Lỗi xảy ra trong quá trình xử lý."}
-
-
